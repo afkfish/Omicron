ddf60cabdaf4b9f8a4693b255f98a0963c372e0c
afkfish <63806903+afkfish@users.noreply.github.com>
2024-03-04 13:46:02 +0100

    scraping, delete, cleanup

---
+++ Omicron/Views/PopOverWebView.swift
@@ -0,0 +1,18 @@
+//
+//  PopOverWebView.swift
+//  Omicron
+//
+//  Created by Beni Kis on 04/03/2024.
+//
+
+import SwiftUI
+
+struct PopOverWebView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PopOverWebView()
+}
